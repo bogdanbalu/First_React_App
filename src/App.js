5b274d11024188bc0a81c8be95b55c07abf7b984
Bogdan Balu <bogdan_va2006@yahoo.com>
2022-10-16 21:34:04 +0300

    React props in progress

--- src/App.js
+++ src/App.js
@@ -3,50 +3,34 @@ import './App.css';
 import TaskCard from './components/task-card/TaskCard';
 
 function App() {
-  const result = [];
-  const data = [
+  const details = [
     {
       id: "T-1",
       status: "To do",
       content: "1. Create a Design System for Enum Workspace.",
       dueDate: new Date(2022, 5, 22)
     },
+
     {
       id: "T-2",
-      status: "In Progress",
-      content: "2. Create a Design System for Enum Workspace.",
-      dueDate: new Date(2022, 5, 23)
+      status: "To do",
+      content: "1. Create a Design System for Enum Workspace.",
+      dueDate: new Date(2022, 5, 22)
     },
+
     {
       id: "T-3",
-      status: "Completed",
-      content: "3. Create a Design System for Enum Workspace.",
-      dueDate: new Date(2022, 5, 24)
-    }
+      status: "To do",
+      content: "1. Create a Design System for Enum Workspace.",
+      dueDate: new Date(2022, 5, 22)
+    },
   ];
 
   return ( 
     <div className="app-container">
-      <TaskCard 
-        id={data[0].id}
-        status={data[0].status}
-        content={data[0].content}
-        dueDate={data[0].dueDate}
-      />
-
-      <TaskCard 
-        id={data[1].id}
-        status={data[1].status}
-        content={data[1].content}
-        dueDate={data[1].dueDate}
-      />
-
-      <TaskCard 
-        id={data[2].id}
-        status={data[2].status}
-        content={data[2].content}
-        dueDate={data[2].dueDate}
-      />
+      <TaskCard data={details[0]} />
+      <TaskCard data={details[1]} />
+      <TaskCard data={details[2]} />
     </div>
   );
 }
--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -4,20 +4,20 @@ const TaskCard = (props) => {
     return (
         <div className="card-wrapper">
             <div className="card-header">
-                <p className="task-id">{props.id}</p>
+                <p className="task-id">{props.data.id}</p>
                 <div className="badge">
-                    <p>{props.status}</p>
+                    <p>{props.data.status}</p>
                 </div>
             </div>
 
             <div className="card-content">
-                <p>{props.content}</p>
+                <p>{props.data.content}</p>
             </div>
 
             <div className="card-footer">
                 <div>
                     <p className="due-date">Due Date</p>
-                    <p className="date">{props.dueDate.toLocaleDateString()}</p>
+                    <p className="date">{props.data.dueDate.toLocaleDateString()}</p>
                 </div>
             </div>
         </div>
