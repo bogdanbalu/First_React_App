332dcad5de13f2f17c1dcb24f454bf5140c702cc
Bogdan Balu <bogdan_va2006@yahoo.com>
2022-10-16 19:42:02 +0300

    update

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,25 +1,29 @@
 import "./TaskCard.css";
 
 const TaskCard = () => {
+    const taskId = "T-2";
+    const taskStatus = "Todo";
+    const taskContent = "Create a Design System for Enum Workspace.";
+    const taskDueDate = "Due Date";
+    const taskDate = new Date(2022, 5, 23);
+
     return (
         <div className="card-wrapper">
             <div className="card-header">
-                <p className="task-id">T-1</p>
+                <p className="task-id">{taskId}</p>
                 <div className="badge">
-                    <p>Todo</p>
+                    <p>{taskStatus}</p>
                 </div>
             </div>
 
             <div className="card-content">
-                <p>
-                    Create a Design System for Enum Workspace.
-                </p>
+                <p>{taskContent}</p>
             </div>
 
             <div className="card-footer">
                 <div>
-                    <p className="due-date">Due Date</p>
-                    <p className="date">24/ 01 /2021</p>
+                    <p className="due-date">{taskDueDate}</p>
+                    <p className="date">{taskDate.toLocaleDateString()}</p>
                 </div>
             </div>
         </div>
